5798a47d9c53f68dd322909a06e21f20a855c55b
AdrianoGamba <adriano-gamba@hotmail.com>
2019-09-15 18:46:27 -0300

    Lab4 | Inicio e colocação das imagens, alterações pequenas nos códigos

--- Labs/Lab4/Atividade1.m
+++ Labs/Lab4/Atividade1.m
@@ -1,31 +1,37 @@
 clear;
 close all;
 
-%Veja o help da função rcosfir para compreender os parâmetros a seguir
+% Variables
 fim=5;
 T=1;
 f=10;
 delta=T/f;
-color = 'rgb'
+color = 'rgb';
 
 %% RAIZ COSSENO LEVANTADO
 for alfa = 0:0.5:1
     f=10;
     fprintf('alfa = %i\n',alfa); 
-    h=rcosfir(alfa,fim,f,T,'sqrt');
+    
+    % Resposta ao impulso - Raiz Cosseno Levantado
+    h = rcosfir(alfa,fim,f,T,'sqrt');
+    
+    % Plot
     figure(1);
     eixo=-fim*T:delta:fim*T;
     plot(eixo,h,color((2*alfa+1)))
     hold on;
     xlabel('tempo (intervalo de símbolo)')
     title('Raiz cosseno levantado')
-    legend('\alpha=0.5');
     grid
 
+    % Bits transmitidos
     bits=[1 0 0 1 0];
     s=2*bits-1;
     s_up=upsample(s,f);
     x=conv(s_up,h);
+    
+    % Plot
     figure(2)
     eixo=-fim*T:delta:(fim+(length(bits)-1)+(1-1/f))*T;
     plot(eixo,x,color((2*alfa+1)));
@@ -67,45 +73,53 @@ for alfa = 0:0.5:1
         f(conta)=ff;
     end 
 
+    % Plot
     figure(3)
     plot(f,2*W*H,color((2*alfa+1)))
     hold on;
     axis([-3 3 0 1.1])
     grid
     title('Resposta em Frequência - Raiz cosseno levantado')
-%    legend('\alpha=0.5');
     xlabel('\times R/2 (Hz)');
     ylabel('H(f)');
 
 end
 H = 0;
+
 %% COSSENO LEVANTADO
 for alfa = 0:0.5:1
     f=10;
     fprintf('alfa = %i\n',alfa); 
+    
+    % Resposta ao impulso - Cosseno Levantado
     h=rcosfir(alfa,fim,f,T);
+    
+    % Plot
     figure(4);
     eixo=-fim*T:delta:fim*T;
     plot(eixo,h,color((2*alfa+1)))
     hold on;
     xlabel('tempo (intervalo de símbolo)')
     title('Cosseno levantado')
-    legend('\alpha=0.5');
     ylim([-0.3 1.2])
     grid
 
+    % Bits transmitidos
     bits=[1 0 0 1 0];
     s=2*bits-1;
     s_up=upsample(s,f);
     x=conv(s_up,h);
+    
+    % Plot
     figure(5)
     eixo=-fim*T:delta:(fim+(length(bits)-1)+(1-1/f))*T;
     plot(eixo,x,color((2*alfa+1)));
+    hold on;
     grid
     xlabel('tempo (intervalo de símbolo)')
     title('Sinal transmitido - Cosseno levantado')
     xlim([-3 8])
-    ylim([-1.2 1.2])
+    ylim([-2 2])
 
 
     %% Resposta em Frequencia Usando a Expressão Exata da Transformada de Fourier (pode ser feito via FFT a partir do pulso no tempo também)
@@ -138,16 +152,39 @@ for alfa = 0:0.5:1
         H(conta)=0;
         f(conta)=ff;
     end 
-
+    
+    % Plot
     figure(6)
     plot(f,2*W*H,color((2*alfa+1)))
     hold on;
     axis([-3 3 0 1.1])
     grid
     title('Resposta em Frequência - Cosseno levantado')
-%    legend('\alpha=0.5');
     xlabel('\times R/2 (Hz)');
     ylabel('H(f)');
 
 end
+
+% Plots' legends
+figure(6);
+legend('\alpha=0','\alpha=0.5','\alpha=1');
+figure(5);
+legend('\alpha=0','\alpha=0.5','\alpha=1');
+figure(4);
+legend('\alpha=0','\alpha=0.5','\alpha=1');
+figure(3);
+legend('\alpha=0','\alpha=0.5','\alpha=1');
+figure(2);
+legend('\alpha=0','\alpha=0.5','\alpha=1');
+figure(1);
+legend('\alpha=0','\alpha=0.5','\alpha=1');
+
+% Save figures
+% saveas(figure(1),'Pulso_Raiz_Cosseno.bmp')
+% saveas(figure(2),'Sinal_Transmitido_Raiz_Cosseno.bmp')
+% saveas(figure(3),'Frequencia_Raiz_Cosseno.bmp')
+% saveas(figure(4),'Pulso_Cosseno.bmp')
+% saveas(figure(5),'Sinal_Transmitido_Cosseno.bmp')
+% saveas(figure(6),'Frequencia_Cosseno.bmp')
+
 
